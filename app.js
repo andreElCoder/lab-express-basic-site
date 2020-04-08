c75b3eddd78ec9590921f17812133d6bac39e398
Andre R <andre.rabaca94@gmail.com>
2020-04-08 21:28:04 +0100

    Done

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require ("express")
+const app = express()
+
+app.use (express.static('public'))
+
+app.get('/home',(request,response) =>{
+    response.sendFile(`${__dirname}/views/home-page.html`);
+});
+
+app.get('/about',(request,response) =>{
+    response.sendFile(`${__dirname}/views/about.html`);
+});
+app.get('/works',(request,response) =>{
+    response.sendFile(`${__dirname}/views/works.html`);
+});
+
+
+app.listen(3000,() => console.log("My first app is listening"))
